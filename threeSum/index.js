185204d849d6af92d80c64dcf61d90535e5aa0c4
guziyi <guziyi@plantdata.cn>
2022-04-20 09:55:48 +0800

    feat:三数之和

--- threeSum/index.js
+++ threeSum/index.js
@@ -1,14 +1,38 @@
 const arr = [-1,0,1,2,-1,-4]
+//[ -4, -1, -2, 0, 1, 1 ] l=1, r=5; -4,-1,
+// i            l           r
+    // -4          -1          1
 //排序+双指针
 var threeSum = function(nums) {
-    let nums2 = nums.sort((a,b) => a-b)
-    console.log(nums2);
-    let l = 0, r = nums2.length - 1;
-    for(let i=0;i<nums2.length;i++) {
-        
+    let ret = [];
+    if(nums == null || nums.length < 3) return ret
+    const len = nums.length
+    nums.sort((a,b) => a-b)
+    for(let i=0;i<len;i++) {
+        if(nums[i] > 0) break
+        if(i>0 && nums[i] === nums[i-1]) continue
+        let l = i+1, r=len - 1
+        while(l < r) {
+            let sum = nums[i] + nums[l] + nums[r]
+            if(sum === 0) {
+                ret.push([nums[i], nums[l], nums[r]])
+                while(l<r && nums[l] === nums[l+1]) l++
+                while(l<r && nums[r] === nums[r-1]) r--
+                l++
+                r--
+            }
+            else if(sum < 0) {
+                l++
+            }
+            else {
+                r--
+            }
+        }
     }   
+    return ret
 };
-threeSum(arr);
+let result = threeSum(arr);
+console.log(result);
 
 const twoSum = function(nums, target = 0) {
     const map = new Map()
