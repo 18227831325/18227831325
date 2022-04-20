013ef8359fdcf4d016fcc7335914c0d33f1eefc9
guziyi <guziyi@plantdata.cn>
2022-04-20 09:58:31 +0800

    test

--- apply/apply.js
+++ apply/apply.js
@@ -8,3 +8,20 @@ function apply(fn,obj,arr){
   delete obj.temp
   return result
 }
+
+//for和break的区别 break是完全结束一个循环，continue是终止本次循环
+
+let str ='hello'
+for(const item of str) {
+  if(item === 'l') {
+    break;
+  }
+  // console.log(item);
+}
+
+for(const item of str) {
+  if(item === 'l') {
+    continue;
+  }
+  console.log(item);
+}
